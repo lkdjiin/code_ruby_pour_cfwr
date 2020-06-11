1e4f6e1d1a65c8329a2536b3139ff65b76c20758
Xavier Nayrac <xavier.nayrac@gmail.com>
2020-06-11 11:07:11 +0200

    Update chapter 4 to ruby 2.7.1

--- chapter04/chapter04.rb
+++ chapter04/chapter04.rb
@@ -22,20 +22,20 @@ Bundler.require(:default)
 # Préciser la version de Ruby
 # Fichier Gemfile
 source "https://rubygems.org/"
-ruby "2.4.0"
+ruby "2.7.1"
 gem 'rack'
 
 # ---
 # Préciser la version d'une gem
 # Fichier Gemfile
 source "https://rubygems.org/"
-ruby "2.4.0"
+ruby "2.7.1"
 gem 'rack', '= 2.0.1'
 
 # ---
 # Inclure puma dans le Gemfile
 # Fichier Gemfile
 source "https://rubygems.org/"
-ruby "2.4.0"
-gem 'rack', '~> 2.0.1'
+ruby "2.7.1"
+gem 'rack', '~> 2.2.0'
 gem 'puma'
--- chapter04/chapter04.sh
+++ chapter04/chapter04.sh
@@ -9,9 +9,9 @@ gem install bundler
 cat Gemfile.lock 
 
 # ---
-# Erreur due à une version d
+# Erreur due à une version de Ruby absente
 rackup
-#=> Your Ruby version is 2.3.3, but your Gemfile specified 2.4.0
+#=> Your Ruby version is 2.4.0, but your Gemfile specified 2.7.1
 
 # ---
 # Recherche naïve
